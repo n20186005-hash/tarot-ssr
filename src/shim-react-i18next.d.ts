fa99c6b409a7b576447b8e36e5931ff3db60b74a
n20186005-hash <n20186005@gmail.com>
2026-01-28 19:26:15 +0800

    1
    检查并提交这 3 个关键文件 ：
    
    1. src/shim-react-i18next.d.ts (这是一个新文件，如果不提交它，构建就会找不到定义)
    2. tsconfig.app.json (我修改了它，让它去使用上面的 shim 文件)
    3. src/vite-env.d.ts (我修改了它，移除了冲突的声明)

---
+++ src/shim-react-i18next.d.ts
@@ -0,0 +1,7 @@
+export const useTranslation: any;
+export const initReactI18next: any;
+export const Trans: any;
+export const Translation: any;
+export const withTranslation: any;
+export const I18nextProvider: any;
+export const I18nContext: any;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,4 +3,3 @@
 declare module 'react-helmet-async';
 declare module 'i18next';
 declare module 'i18next-browser-languagedetector';
-declare module 'react-i18next';
